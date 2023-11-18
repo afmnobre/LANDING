f1566d4e067d482f4620e9c46a75c7df46e63ea5
afmnobre <lucas_guitar1987@hotmail.com>
2023-11-17 23:49:29 -0300

    Incluindo Aula 12.

---
+++ PESSOAL/VAPORHOLE/php/Exemplos/Aula_11/Form_Fetch_Combobox/estados.php
@@ -0,0 +1,26 @@
+<?php
+
+header('content-type: application/json');
+
+$regiao_selecionada = $_REQUEST['regiao_selecionada'];
+
+$estados = [
+		'centro-oeste' => [
+			['id' => 'DF', 'nome' => 'Distrito Federal'],
+			['id' => 'GO', 'nome' => 'Goiás'],	
+			['id' => 'MT', 'nome' => 'Mato Grosso'],
+			['id' => 'MS', 'nome' => 'Mato Grosso do Sul']
+		],
+		'sul' => [
+			['id' => 'PR', 'nome' => 'Paraná'],
+			['id' => 'RS', 'nome' => 'Rio Grande do Sul'],
+			['id' => 'SC', 'nome' => 'Santa Catarina']
+		]   
+	]; 
+
+echo json_encode($estados[$regiao_selecionada]);
+
+
+
+
+?>
