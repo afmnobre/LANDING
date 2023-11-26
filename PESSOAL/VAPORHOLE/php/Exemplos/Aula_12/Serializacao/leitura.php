1d33700f2af1d382c9e53bead10195f715ba0bc6
afmnobre <lucas_guitar1987@hotmail.com>
2023-11-26 00:34:46 -0300

    Resolvendo BUG.

--- PESSOAL/VAPORHOLE/php/Exemplos/Aula_12/Serializacao/leitura.php
+++ PESSOAL/VAPORHOLE/php/Exemplos/Aula_12/Serializacao/leitura.php
@@ -8,9 +8,9 @@ $turmas = unserialize($serializacao);
 echo "<table border>";
 foreach($turmas as $turma){
 	echo "<tr>";
-	echo "<td>{$turma->nome}</td>";
-	echo "<td>{$turma->data->format('d/m/Y')}</td>";
-	echo "<td>". implode(", ", array_column($turma->alunos, 'nome'))."</td>";
+		echo "<td>{$turma->nome}</td>";
+		echo "<td>{$turma->data->format('d/m/Y')}</td>";
+		echo "<td>". implode(", ", array_column($turma->alunos, 'nome'))."</td>";
 	echo "</tr>";
 }
 echo "</table>";
